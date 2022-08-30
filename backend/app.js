e46cbe7c5859d614198b35d1a66429f3f4af5348
Владимир Куклин <privet1vovik@gmail.com>
2022-08-30 13:08:35 +0300

    cors?

--- backend/app.js
+++ backend/app.js
@@ -18,11 +18,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const corsOptions = {
-  origin: 'https://expressmesto.students.nomoredomains.sbs',
-  credentials: true,
-  optionsSuccessStatus: 200,
-};
+// const corsOptions = {
+//   origin: 'https://expressmesto.students.nomoredomains.sbs',
+//   credentials: true,
+//   optionsSuccessStatus: 200,
+// };
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -38,7 +38,7 @@ app.use((req, res, next) => {
 });
 
 app.use(requestLogger);
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
